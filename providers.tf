cbf55031d20728bfb6eea872ba9b4789ee228fbc
deepak-kr7 <deepaknsn7@gmail.com>
2026-08-12 22:33:09 +0530

    feat(terraform): configure AzureRM remote backend in providers.tf

--- providers.tf
+++ providers.tf
@@ -6,6 +6,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "cicd_test"
+    storage_account_name = "storagecicd12"
+    container_name       = "cicdcontainer"
+    key                  = "agenticailog.tfstate"
+  }
 }
 
 provider "azurerm" {
